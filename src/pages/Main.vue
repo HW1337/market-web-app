302516589de225e29f66bdc30b04f716d8aff8ca
HW1337 <kskobelin@ya.ru>
2024-03-26 22:23:48 +0700

    started implementing vuex

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-    <SearchHeader @search="updateSearchTerm" @cart="makeCartVisible" :cartQuantity="cartQuantity"/>
-    <ProductList :products="products" :searchQuery="searchTerm" @addToCart="addToCart"/>
-    <cart-dialog v-model:show="cartVisible" ><Cart v-model:show="cartVisible" :cart="cart" @refreshCartQuantity="refreshCartQuantity"/></cart-dialog>
+    <SearchHeader @cart="makeCartVisible"/>
+    <ProductList :products="products"/>
+    <cart-dialog v-model:show="cartVisible" ><Cart v-model:show="cartVisible" :cart="cart"/></cart-dialog>
     </div>
   </template>
   
@@ -10,42 +10,28 @@
   import ProductList from '@/components/ProductList.vue';
   import SearchHeader from '@/components/SearchHeader.vue';
   import Cart from '@/components/Cart.vue';
+  import {mapState, mapGetters, mapMutations} from 'vuex'
   
   export default {
     data() {
       return {
-        products: [],
-        searchTerm: '',
         cartVisible: false,
-        cart: [],
-        cartQuantity: 0,
       };
     },
+    computed: {
+    ...mapState(['products', 'searchTerm', 'cart']),
+    ...mapGetters(['cartQuantity']),
+  },
     components: {
       SearchHeader,
       ProductList,
       Cart,
     },
     methods: {
-      updateSearchTerm(term) {
-        this.searchTerm = term;
-      },
       makeCartVisible() {
         this.cartVisible = true;
       },
-      addToCart(item) {
-        const existingItem = this.cart.find(cartItem => cartItem.id === item.id)
-        if (existingItem) {
-          existingItem.quantity++
-        } else {
-          this.cart.push({ ...item, quantity: 1 })
-          
-        }
-        this.cartQuantity = this.cart.reduce((acc, el)=> acc + el.quantity, 0)
-      },
-      refreshCartQuantity () {
-        this.cartQuantity = this.cart.reduce((acc, el)=> acc + el.quantity, 0)
-      }
+      ...mapMutations(['updateSearchTerm', 'addToCart'])
     },
   };
   </script>
